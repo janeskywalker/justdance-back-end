e73ddf4dad15d4ddc60b2f39ea8fc657f7abc6b7
Jane Skywalker <janehuizeng@gmail.com>
2019-09-10 11:16:33 -0700

    hooking up db Studio

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
-// const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 // import the validate funtion, do front-end validation
 const validate = require('../validation/register');
-// const db = require('../models');
+const db = require('../models');
 const mockData = require('./mockData').mockData;
 const uuid = require('uuid');
 
@@ -10,75 +10,75 @@ const uuid = require('uuid');
 const register = (req, res) => {
   const { errors, notValid } = validate(req.body);
 
-  const newUser = {
-    _id: uuid(),
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
+  // const newUser = {
+  //   _id: uuid(),
+  //   username: req.body.username,
+  //   email: req.body.email,
+  //   password: req.body.password,
+  // }
+
+  // mockData.mockUsers.push(newUser);
+
+  // res.send({
+  //   _id: newUser._id,
+  //   username: newUser.username,
+  //   email: newUser.email,
+  // })
+
+  //Validate Form Data
+  if (notValid) {
+    return res.status(400).json({ status: 400, errors });
   }
 
-  mockData.mockUsers.push(newUser);
+  // Verify Account Does Not Already Exist -- email has aready been registered - if user exist
+  db.User.findOne({ email: req.body.email }, (err, foundUser) => {
+    if (err) {
+        return res.status(500).json({
+            status: 500,
+            message: 'Something went wrong. Please try again'
+        });
+    }
 
-  res.send({
-    _id: newUser._id,
-    username: newUser.username,
-    email: newUser.email,
-  })
+    if (foundUser) {
+        return res.status(400).json({
+            status: 400,
+            message: 'Email address has already been registered. Please try again'
+        });
+    }
 
-  // Validate Form Data
-//   if (notValid) {
-//     return res.status(400).json({ status: 400, errors });
-//   }
+    // Generate Salt (Asynchronous callback version)
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) {
+          return res.status(500).json({
+              status: 500,
+              message: 'Something went wrong. Please try again'
+            });
+        }
+      // if (err) throw err;
 
-//   // Verify Account Does Not Already Exist -- email has aready been registered - if user exist
-//   db.User.findOne({ email: req.body.email }, (err, foundUser) => {
-//     if (err) {
-//         return res.status(500).json({
-//             status: 500,
-//             message: 'Something went wrong. Please try again'
-//         });
-//     }
+      // Hash User Password
+      bcrypt.hash(req.body.password, salt, (err, hash) => {
+        if (err) return res.status(500).json({ status: 500, message: 'Something went wrong. Please try again'});
 
-//     if (foundUser) {
-//         return res.status(400).json({
-//             status: 400,
-//             message: 'Email address has already been registered. Please try again'
-//         });
-//     }
+        const newUser = {
+          username: req.body.username,
+          email: req.body.email,
+          password: hash,
+        }
 
-//     // Generate Salt (Asynchronous callback version)
-//     bcrypt.genSalt(10, (err, salt) => {
-//       if (err) {
-//           return res.status(500).json({
-//               status: 500,
-//               message: 'Something went wrong. Please try again'
-//             });
-//         }
-//       // if (err) throw err;
+        // now another query to create a user and save it
+        db.User.create(newUser, (err, savedUser) => {
+          // if (err) return res.status(500).json({ status: 500, message: err});
+          if (err) res.send(err)
+          console.log('registering user')
+          console.log(savedUser)
+          // res.status(201).json({ status: 201, message: 'success', savedUser: savedUser});
+          res.send(savedUser)
+        });
+      });
+    });
 
-//       // Hash User Password
-//       bcrypt.hash(req.body.password, salt, (err, hash) => {
-//         if (err) return res.status(500).json({ status: 500, message: 'Something went wrong. Please try again'});
-
-//         const newUser = {
-//           username: req.body.username,
-//           email: req.body.email,
-//           password: hash,
-//         }
-
-//         // now another query to create a user and save it
-//         db.User.create(newUser, (err, savedUser) => {
-//           // if (err) return res.status(500).json({ status: 500, message: err});
-//           if (err) res.send(err)
-//           console.log('registering user')
-//           console.log(savedUser)
-//           // res.status(201).json({ status: 201, message: 'success', savedUser: savedUser});
-//           res.send(savedUser)
-//         });
-//       });
-//     });
-
-//   });
+  });
 };
 
 
--- controllers/studiosController.js
+++ controllers/studiosController.js
@@ -1,36 +1,38 @@
-// const db = require('../models');
-const mockData = require('./mockData').mockData
+const db = require('../models');
+// const mockData = require('./mockData').mockData
 
 
 // show all studios
 const index = (req, res) => {
-    // db.City.find({}, (error, foundCities) => {
-    //     if (error) return res.status(500).send(error);
-    //     console.log({foundCities})
-    //     // response.sendResponse(res, foundCities);
-    //     res.send(foundCities)
-    // });
+    db.Studio.find({}, (error, foundStudios) => {
+        if (error) return res.status(500).send(error);
+        console.log({foundStudios})
+        // response.sendResponse(res, foundCities);
+        res.send(foundStudios)
+    });
     
-    res.send(mockData.mockStudios)
+    // res.send(mockData.mockStudios)
 }
 
-// show one city, taking id
+// show one studio, taking id
 const show = (req, res) => {
   console.log('requesting one studio')
-//   db.City.findById(req.params.id, (error, foundCity) => {
-//     if (error) return res.status(500).send(error);
-//     // response.sendResponse(res, foundPokemons);
-//     res.send(foundCity)
-//     });
-    const studio = mockData.mockStudios.find((studio) => {
-        return studio._id === req.params.id
-    })
+  db.Studio.findById(req.params.id, (error, foundStudio) => {
+    if (error) return res.status(500).send(error);
+    // response.sendResponse(res, foundPokemons);
+    res.send(foundStudio)
+    });
 
-    if (studio) {
-        res.send(studio)
-    } else {
-        res.status(404).json({ status: 404, message: 'Studio not found' });
-    }
+
+    // const studio = mockData.mockStudios.find((studio) => {
+    //     return studio._id === req.params.id
+    // })
+
+    // if (studio) {
+    //     res.send(studio)
+    // } else {
+    //     res.status(404).json({ status: 404, message: 'Studio not found' });
+    // }
 }
 
 // creating post for a city
