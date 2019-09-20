af6b0bd30783ce3aa2fe9cc0128c144f0f6f4e32
Jane Skywalker <janehuizeng@gmail.com>
2019-09-20 12:16:40 -0700

    updating connect at models index file

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/justdance';
+const MONGO_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/justdance';
 
-mongoose.connect(DB_URL, {
+mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true,
